4bd6e73ef67ebe3a73fb573a6386d5949ffac2bb
KaLuLas <kalulas41@gmail.com>
2019-02-02 17:47:06 +0800

    目录选择 & 评分结果显示 & txt文件格式

--- poll.py
+++ poll.py
@@ -1,5 +1,7 @@
 # [方法（左），方法（右），图片序号，选择（左或右），选择的方法]
 from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
 from PIL import Image, ImageTk
 import random
 import re
@@ -15,39 +17,52 @@ item_to_delete = []
 name = []
 
 
-def get_name():
+def get_name(Event=None):
     name.append(En.get())
     root0.destroy()
+
+
+def get_project_name(Event=None):
+    global project_folder
+    project = project_listbox.get(project_listbox.curselection())
+    project = '\\' + project
+    project_folder += project
+    root_select_project.destroy()
+
+
+def get_original_name(Event=None):
+    global original_folder
+    original = folder_listbox.get(folder_listbox.curselection())
+    original_folder = '\\' + original
+    root_select_original.destroy()
 
 
 # 根据方法与原始图片名，找到该方法对应的图片名
 def pic_name(var, pic):
     file_list = os.listdir(project_folder + '\\' + var)
-    pic += '_'
     for file_name in file_list:
         if pic in file_name:
             return file_name
     return 'error'
-    '''
-    return {
-        'BiGAN': 'step9299_img_' + pic + '.png',
-        'cr': pic + '_0.50_cr.png',
-        'lg': pic + '_0.50_lg.png',
-        'multiop': pic + '_0.50_multiop.png',
-        'osa': pic + '_0.50_osa.png',
-        'qp': pic + '_0.50_qp.png',
-        'sc': pic + '_0.50_sc.png',
-        'scl': pic + '_0.50_scl.png',
-        'sv': pic + '_0.50_sv.png',
-        'warp': pic + '_0.50_warp.png'
-    }.get(var, 'error')  # 'error'为默认返回值，可自设置
-'''
 
 
 # “left”、“right”按钮所对应的指令
 def data_append():
     choice.append(data.get())  # 将用户选择“左”或“右”加入choice列表
     root.destroy()  # 终止本次循环，进入下一组选择
+
+
+def save():
+    method_count = [0 for i in range(method_num)]
+    for i in range(len(list_b)):
+        method_count[list_b[i][4] - 1] += 1
+    if not os.path.exists(project_folder + '/results.txt'):
+        file = open(project_folder + '/results.txt', 'w')
+        file.write(str(method_dict) + '\n')
+        file.close()
+    with open(project_folder + '/results.txt', 'a') as fw:
+        fw.write(' '.join([str(i) for i in method_count]) + ' ' + str(name[0]) + '\n')
+        fw.close()
 
 
 def close_with_saving():
@@ -58,11 +73,7 @@ def close_with_saving():
     for i in range(len(list_b)):
         list_b[i].append(choice[i])
         list_b[i].append(list_b[i][choice[i]])
-    method_count = [0 for i in range(method_num)]
-    for i in range(len(list_b)):
-        method_count[list_b[i][4] - 1] += 1
-    with open(project_folder + '/results.txt', 'a') as fw:
-        fw.write(' '.join([str(i) for i in method_count]) + ' ' + str(name[0]) + '\n')
+    save()
     exit()
 
 
@@ -71,14 +82,42 @@ def close_without_saving():
 
 
 # "exit" 按钮对应的指令
-def exit_window():
-    root_exit = Tk()
-    Label(root_exit, text="是否保存当前评分信息?").grid(row=0, column=1)
-    Button(root_exit, text="yes", width=10, height=1, command=close_with_saving).grid(row=1, column=0)
-    Button(root_exit, text="no", width=10, height=1, command=close_without_saving).grid(row=1, column=2)
-    root_exit.mainloop()
+def on_exit():
+    answer = messagebox.askyesno('Save', '是否保存当前评分信息?')
+    if answer:
+        close_with_saving()
+    else:
+        close_without_saving()
 
 
+def reveal_label():
+    label_2methods = Label(root, text="左图方法：" + method_dict[list_b[i][0]] + "  右图方法："
+                                      + method_dict[list_b[i][1]], width=40, height=1, justify=LEFT, font=("黑体", 11))
+    label_2methods.place(x=0, y=load_0.size[1] + 115)
+
+
+def reveal_result():
+    if not os.path.exists(project_folder + '/results.txt'):
+        messagebox.showerror('Error', '尚无评分记录')
+    else:
+        records = []
+        file = open(project_folder + '/results.txt', 'r')
+        dic = file.readline()
+        dic = re.sub('[{}:\']', '', dic)
+        while True:
+            line = file.readline()
+            if line:
+                records.append(list(line.strip('\n').split(' ')))
+            else:
+                break
+        method_sum = [0 for i in range(method_num)]
+        # method_sum为所有用户的选择
+        for i in range(len(records)):
+            for j in range(method_num):
+                method_sum[j] += int(records[i][j])
+        messagebox.showinfo('Records', dic + '得分情况：' + str(method_sum))
+
+    
 def resize(w_box, h_box, picture):
     width, height = picture.size  # 获取图像的原始大小
     f1 = 1.0 * w_box / width
@@ -90,43 +129,71 @@ def resize(w_box, h_box, picture):
 
 
 root0 = Tk()
+root0.title("login")
 # 昵称不要有空格
-label = Label(root0, text='请输入您的昵称:', anchor='c').grid(row=0)
+label = Label(root0, text='请输入您的昵称（非空）:', anchor='c').grid(row=0)
 En = Entry(root0)
+En.bind('<KeyRelease-Return>', get_name)
 En.grid(row=0, column=1)
 Button(root0, text='确定', anchor='c', width=6, height=1, command=get_name).grid(row=2, column=1)
 root0.mainloop()
-
+# 关闭窗口或者输入为空直接结束程序
+if len(name) == 0 or name[0] == '':
+    exit()
 
 # 用字典为所有方法编号
-# TODO: user select project
-project_folder = '.\RetargetMe_dataset'
+project_folder = '.'
+root_select_project = Tk()
+root_select_project.title('Open Project(Double Click)')
+# WARNING: 项目文件夹中不可包含 '.'
+project_list = [path for path in os.listdir(project_folder) if '.' not in path]
+listbox_len = 5 if len(project_list) < 5 else len(project_list)
+project_listbox = Listbox(root_select_project, width=70, height=listbox_len, selectmode="browse")
+project_listbox['font'] = ('consolas', 12)
+project_listbox['listvariable'] = StringVar(value=project_list)
+project_listbox.pack(side=TOP)
+project_listbox.bind('<Double-Button-1>', get_project_name)
+root_select_project.mainloop()
+# 没有选择项目路径关闭窗口，或者所选文件夹下没有内容，直接退出程序
+if project_folder == '.' or len([path for path in os.listdir(project_folder)]) == 0:
+    exit()
+
+
+# 用字典为所有图片编号
+original_folder = ''
+root_select_original = Tk()
+root_select_original.title('Select folder that contains original images(Double Click)')
+# WARNING: 项目中的方法文件夹中不可包含 '.'
+folder_list = [path for path in os.listdir(project_folder) if '.' not in path]
+listbox_len = 5 if len(folder_list) < 5 else len(folder_list)
+folder_listbox = Listbox(root_select_original, width=70, height=listbox_len, selectmode="browse")
+folder_listbox['font'] = ('consolas', 12)
+folder_listbox['listvariable'] = StringVar(value=folder_list)
+folder_listbox.pack(side=TOP)
+folder_listbox.bind('<Double-Button-1>', get_original_name)
+root_select_original.mainloop()
+# 没有选择项目路径关闭窗口，或者所选文件夹下没有内容，直接退出程序
+if original_folder == '' \
+        or len([path for path in os.listdir(project_folder + original_folder)]) == 0:
+    exit()
+
 path_methods = project_folder
 method_list = os.listdir(path_methods)
 method_list = [w for w in method_list if not re.search('[\.]', w)]
-method_list.remove('original_image')
+method_list.remove(original_folder[1:])
 method_dict = {}
+
 for i in range(1, len(method_list) + 1):
     method_dict[i] = method_list[i - 1]
-print(method_dict)
-
-# 用字典为所有图片编号
-# TODO: user select original_image
-original_folder = '\original_image'
 path_pictures = project_folder + original_folder
 picture_list = os.listdir(path_pictures)
 picture_list = [re.sub('\.png', '', w) for w in picture_list]
 picture_dict = {}
 for i in range(1, len(picture_list) + 1):
     picture_dict[i] = picture_list[i - 1]
-print(picture_dict)
 
 method_num = len(method_list)  # 方法数量
 picture_num = len(picture_list)  # 图片数量
-# TODO: GUI output
-# print("method number:" + str(method_num))
-# print("picture number:" + str(picture_num))
-
 
 for k in range(1, picture_num + 1):
     list_a = []
@@ -154,7 +221,7 @@ note_s = '''用户对方法1结果图、方法2结果图进行偏好选择，选
 item_to_delete = [item for item in list_b]
 
 for i in range(len(list_b)):
-    path_originalpic = project_folder + original_folder + "\\" + picture_dict[list_b[i][2]] + ".png"
+    path_originalpic = project_folder + original_folder + "\\" + picture_dict[list_b[i][2]] + '.png'
     path_leftpic = project_folder + "\\" + method_dict[list_b[i][0]] + "\\" + pic_name(method_dict[list_b[i][0]],
                                                                                        picture_dict[list_b[i][2]])
     path_rightpic = project_folder + "\\" + method_dict[list_b[i][1]] + "\\" + pic_name(method_dict[list_b[i][1]],
@@ -187,7 +254,7 @@ for i in range(len(list_b)):
         img_2.image = render_2
         img_2.place(x=load_0.size[0] + load_1.size[0] + 200, y=0)  # 设置图片放置位置
 
-        exit_button = Button(root, text="Exit", font=("黑体", 10), width=10, height=1, command=exit_window)
+        exit_button = Button(root, text="Exit", font=("黑体", 10), width=10, height=1, command=on_exit)
         # K： 显示当前的评分进度
         exit_button.pack(side=BOTTOM, pady=10)
         progress = "\n" + "[Progress]: " + str(i) + " / " + str(len(list_b))
@@ -205,13 +272,24 @@ for i in range(len(list_b)):
         root.geometry('%dx%d' % (width, height))
         root.title("Choose The Better One")
 
-        label_original = Label(root, text="原始图像", width=int(load_0.size[0] / 8), height=1, bg="grey", font=("黑体", 11))
-        label_m1 = Label(root, text="方法1结果图", width=int(load_1.size[0] / 8), height=1, bg="grey", font=("黑体", 11))
-        label_m2 = Label(root, text="方法2结果图", width=int(load_2.size[0] / 8), height=1, bg="grey", font=("黑体", 11))
+        label_original = Label(root, text="原始图像：" + picture_dict[list_b[i][2]], width=int(load_0.size[0] / 8), height=1, bg="LightSteelBlue", font=("黑体", 11))
+        label_m1 = Label(root, text="方法1结果图", width=int(load_1.size[0] / 8), height=1, bg="LightSteelBlue", font=("黑体", 11))
+        label_m2 = Label(root, text="方法2结果图", width=int(load_2.size[0] / 8), height=1, bg="LightSteelBlue", font=("黑体", 11))
+        label_picture_num = Label(root, text="原始图片总数：" + str(picture_num), width=25, height=1, justify=LEFT, font=("黑体", 11))
+        label_method_num = Label(root, text="生成方法总数：" + str(method_num), width=25, height=1, justify=LEFT, font=("黑体", 11))
+        
         label_original.place(x=0, y=load_0.size[1] + 1)
         label_m1.place(x=load_0.size[0] + 100, y=load_1.size[1] + 1)
         label_m2.place(x=load_0.size[0] + load_1.size[0] + 200, y=load_2.size[1] + 1)
-
+        label_picture_num.place(x=0, y=load_0.size[1] + 40)
+        label_method_num.place(x=0, y=load_0.size[1] + 60)
+        
+        reveal_button = Button(root, text="点击查看左右图对应方法", width=25, height=1, font=("黑体", 11),
+                               bg="LightGrey", command=reveal_label)
+        reveal_button.place(x=35, y=load_0.size[1] + 90)
+        result_button = Button(root, text="查看各类方法得分情况", width=23, height=1,  font=("黑体", 11),
+                               bg="LightGrey", command=reveal_result)
+        result_button.place(x=35, y=load_0.size[1] + 150)
         data = IntVar()
         # 设置“左”与“右”按钮
         left_button = Radiobutton(root, text="Left", width=int(load_1.size[0] / 9), height=1, variable=data, value=0,
@@ -227,7 +305,7 @@ for i in range(len(list_b)):
     else:
         pass
         # item_to_delete.append(list_b[i])
-
+'''
 for i in item_to_delete:
     list_b.remove(i)
 
@@ -267,5 +345,4 @@ print(method_sum)
 method_num = 1
 for score in method_sum:
     print("Score for method", method_dict[method_num], ": ", score)
-    method_num += 1
-
+    method_num += 1 '''
